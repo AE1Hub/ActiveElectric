9e9a980e4bcde812b247a8d765ca5e49348acf09
Jon Kimmel <jonkimmel16@gmail.com>
2021-04-21 20:12:19 -0700

    router_setup

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import Contact from "../ContactUs/contact";
+import OurStory from "../OurStory/ourStory";
+import Services from "../Services/services";
 
-function App() {
-  return (
-    <main className="App">
-        <div>
-          <p> test App</p>
-        </div>
-    </main>
-  );
+export default function App() {
+	const renderNavRoutes = () => {
+		return (
+				<Switch>
+					<Route exact path='/' />
+					<Route path='/contact' component={Contact} />
+					<Route path='/ourStory' component={OurStory} />
+					<Route path='/services' component={Services} />
+				</Switch>
+		);
+	};
+
+	return (
+			<main className='app'>
+				<nav className='appNav'>{renderNavRoutes()}</nav>
+				<div>
+					<p>test</p>
+				</div>
+			</main>
+	);
 }
-
-export default App;
