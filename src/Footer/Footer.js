c53ed71e33338987118fc013d07109901bd783cd
Jon Kimmel <Jonkimmel16@gmail.com>
2022-05-20 19:48:47 -0700

    emailUpdate

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,18 +1,17 @@
-import React from "react";
-import "./Footer.css";
+import React from 'react';
+import './Footer.css';
 
 export default function Footer() {
-	return (
-		<footer className='footer'>
-			<div className='miscInfo'>
-				<p>Active Electric Co. &#169;</p>
-				<p>Tel 602.549.5052</p>
-		                <p>Email info@activelectric.com</p>
-		                <p>Email David@activelectric.com</p>
-				<p>21419 S. 154th St.</p>
-				<p>Gilbert, AZ 85298</p>
-				<p>ROC 328575</p>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className='footer'>
+      <div className='miscInfo'>
+        <p>Active Electric Co. &#169;</p>
+        <p>Tel 602.549.5052</p>
+        <p>Email info@activelectric.com</p>
+        <p>21419 S. 154th St.</p>
+        <p>Gilbert, AZ 85298</p>
+        <p>ROC 328575</p>
+      </div>
+    </footer>
+  );
 }
